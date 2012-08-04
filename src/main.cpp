5b48f14efe9e0ba8abc6a87a24348692c2a36c70
Maxence Adnot <adnot.maxence@gmail.com>
2012-08-05 00:57:54 +0200

    run script

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,10 @@ using namespace std;
 int main()
 {
 
-  string mesg = "Hello World !";
+  string mesg = "Game Of Life";
   initscr();
   mvprintw(LINES / 2, (COLS - mesg.length()) / 2, "%s" , mesg.c_str());
+  mvprintw(LINES - 1.0, 0, "Kevin Bacas & Maxence Adnot");
   refresh();
   getch();
   endwin(); 
